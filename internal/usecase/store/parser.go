3fc0e3ddd36317377c1b95197c6b942aa444534e
Fabrício Girardi Andreis <fabricio.andreis@gmail.com>
2023-05-04 13:26:55 -0300

    chore: add comments to clarify main steps of the parser algorithm

--- internal/usecase/store/parser.go
+++ internal/usecase/store/parser.go
@@ -21,34 +21,34 @@ func (p *parser) parseStream(jsonStream io.Reader, ports chan<- domain.Port, err
 	defer close(errs)
 
 	dec := json.NewDecoder(jsonStream)
-	_, err := dec.Token()
+	_, err := dec.Token() // Consume opening curly braces
 	if err != nil {
 		p.handleError(err, errs)
 		return
 	}
 	for dec.More() {
+		// Port ID is a different token on each item, therefore we just consume it as a string token
 		tokenID, err := dec.Token()
 		if err != nil {
 			p.handleError(err, errs)
 			return
 		}
-
 		id, ok := tokenID.(string)
 		if !ok {
 			p.handleError(errors.New("unable to cast token ID to string"), errs)
 			return
 		}
-
 		port := domain.Port{ID: id}
+
+		// Consume remaining part of the item and unmarshal it into Port entity
 		err = dec.Decode(&port)
 		if err != nil {
 			p.handleError(err, errs)
 			return
 		}
-
 		ports <- port
 	}
-	_, err = dec.Token()
+	_, err = dec.Token() // Consume closing curly braces
 	if err != nil {
 		p.handleError(err, errs)
 		return
