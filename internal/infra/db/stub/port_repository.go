33653415bb483d203ad3cb7a8f95e7c270fafae0
Fabrício Girardi Andreis <fabricio.andreis@gmail.com>
2023-05-04 21:59:37 -0300

    chore: add unit tests for stub port repository, add stub port repository implementation

---
+++ internal/infra/db/stub/port_repository.go
@@ -0,0 +1,34 @@
+package stub
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	"github.com/fabricioandreis/ports-app/internal/domain"
+)
+
+type PortRepository struct {
+	db sync.Map
+}
+
+func NewPortRepository() *PortRepository {
+	return &PortRepository{db: sync.Map{}}
+}
+
+func (repo *PortRepository) Get(ctx context.Context, portID string) (*domain.Port, error) {
+	res, ok := repo.db.Load(portID)
+	if !ok {
+		return nil, nil
+	}
+	port, ok := res.(domain.Port)
+	if !ok {
+		return nil, errors.New("unable to cast result as port")
+	}
+	return &port, nil
+
+}
+func (repo *PortRepository) Put(ctx context.Context, port domain.Port) error {
+	repo.db.Store(port.ID, port)
+	return nil
+}
