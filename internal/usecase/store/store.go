fb4e0ec44330c3f61f778c00050ac45ec57ec7da
Fabrício Girardi Andreis <fabricio.andreis@gmail.com>
2023-05-04 22:09:21 -0300

    feat: add unit test for store port use case, add store port usecase implementation

---
+++ internal/usecase/store/store.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"context"
+	"io"
+
+	"github.com/fabricioandreis/ports-app/internal/contracts/repository"
+	"github.com/fabricioandreis/ports-app/internal/domain"
+)
+
+type StoreUsecase struct {
+	repoPort repository.Port
+}
+
+func NewStoreUsecase(repoPort repository.Port) *StoreUsecase {
+	return &StoreUsecase{repoPort}
+}
+
+func (usc *StoreUsecase) Store(ctx context.Context, jsonStream io.Reader) (int, error) {
+	parser := newParser()
+	ports := make(chan domain.Port)
+	errs := make(chan error)
+	go parser.parseStream(ctx, jsonStream, ports, errs)
+
+	count := 0
+
+	for {
+		select {
+		case port, ok := <-ports:
+			if !ok {
+				return count, nil
+			}
+			usc.repoPort.Put(ctx, port)
+			count++
+		case err := <-errs:
+			return count, err
+		}
+	}
+}
---
+++ internal/usecase/store/store_test.go
@@ -0,0 +1,52 @@
+package store_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/fabricioandreis/ports-app/internal/domain"
+	"github.com/fabricioandreis/ports-app/internal/infra/db/stub"
+	"github.com/fabricioandreis/ports-app/internal/usecase/store"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	portRioGrande = domain.Port{
+		ID:          "BRRIG",
+		Code:        "35173",
+		Name:        "RioGrande",
+		City:        "RioGrande",
+		Province:    "RioGrandedoSul",
+		Country:     "Brazil",
+		Timezone:    "America/Sao_Paulo",
+		Alias:       []string{},
+		Coordinates: []float32{-52.1075802, -32.0353776},
+		Regions:     []string{},
+		Unlocs:      []string{"BRRIG"},
+	}
+)
+
+func TestStoreUsecase(t *testing.T) {
+	t.Run("Should be able to store Ports from input JSON stream into the database", func(t *testing.T) {
+		repoPort := stub.NewPortRepository()
+		jsonStream := strings.NewReader(`{"BRRIG":{"name":"RioGrande","city":"RioGrande","province":"RioGrandedoSul","country":"Brazil","alias":[],"regions":[],"coordinates":[-52.1075802,-32.0353776],"timezone":"America/Sao_Paulo","unlocs":["BRRIG"],"code":"35173"}}`)
+		ctx := context.Background()
+
+		usecase := store.NewStoreUsecase(repoPort)
+		output, err1 := usecase.Store(ctx, jsonStream)
+		found, err2 := repoPort.Get(ctx, "BRRIG")
+
+		assert.NoError(t, err1)
+		assert.NoError(t, err2)
+		assert.Equal(t, 1, output)
+		require.NotNil(t, found)
+
+		if !cmp.Equal(portRioGrande, *found) {
+			assert.Fail(t, fmt.Sprintf("port not as expected: %s", cmp.Diff(portRioGrande, *found)))
+		}
+	})
+}
